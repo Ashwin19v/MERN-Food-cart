1cf3446055f5a3163909283ee6afeb9eb0c80dac
Ashwin19v <ashwinash190105@gmail.com>
2026-04-14 18:31:26 +0530

    fixed:bugs

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://mern-food-cart.vercel.app",
+    ],
   },
 });
 
@@ -61,7 +65,11 @@ io.on("connection", (socket) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://mern-food-cart.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
